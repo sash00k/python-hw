64f67a0d75439e51d46210ec408d6ce92d37af26
sash00k <s.a.c.h.a.b@yandex.ru>
2022-10-06 15:47:45 +0600

    1st task add - fix

--- 1/adittional/bit_counter.py
+++ 1/adittional/bit_counter.py
@@ -10,36 +10,19 @@
 # Пример: 10 -> 2 (т.к. 10 - это 0…1010)
 # -123 -> 3 (т.к. -123 - это 1…0000101)
 
-def count_slow(num: int) -> int:
+def count(num: int) -> int:
+    i = 0
     counter = 0
-
-    if num < 0:
-        counter += 1
-        num = -num
-
-        i = 0 # тут я рассчитываю на то, что правильно понял смысл инвертирования бит в двоичном представлении
-        while True:
-            if 2**i >= num:
-                num = 2**i - num
-                break
-            i += 1
-
-    while num > 0:
-        if num % 2 == 1:
+    while True:
+        check = num & (2 ** i)
+        if check > 0:
             counter += 1
-        num //= 2
-
+        if abs(num) < 2 ** i:
+            break
+        i += 1
     return counter
-
-
-def count_low(num: int) -> int:
-    pass
-
-
-def limited_bin(x):
-  print(bin(x & 0xffffffff))
 
 
 if __name__ == '__main__':
     for number in (1, 3, 10, -123, -9, -7, -128, -16):
-        print(number, '->', count_slow(number))
+        print(number, '->', count(number))
