261dc166f9e34fdee7335eb4c16bb727ba66c0f9
sash00k <s.a.c.h.a.b@yandex.ru>
2022-10-27 16:30:12 +0400

    add deprecated.py

--- 3/flatten.py
+++ 3/flatten.py
@@ -1,6 +1,8 @@
 # Реализовать функцию flatten, распаковывающую
 # список, элементы которого могут являться
 # списками. Опционален параметр глубины распаковки
+from functools import reduce
+
 
 def flatten(lst: list, depth: int = -1) -> list:
     was_smth_unpacked = False
@@ -20,8 +22,14 @@ def flatten(lst: list, depth: int = -1) -> list:
     return flatten(lst_unpacked, depth)
 
 
+def func(x, y) -> list:
+
+    return [x, y]
+
+
 if __name__ == '__main__':
     arr = [1, 2, [3, [4]], 5]
-    print(flatten(arr)) # [1, 2, 3, 4, 5]
-    print(flatten(arr, depth=1)) # [1, 2, 3, [4], 5]
-    print(flatten(arr, depth=2)) # [1, 2, 3, 4, 5]
+    # print(flatten(arr)) # [1, 2, 3, 4, 5]
+    print(reduce(func, arr))
+    # print(flatten(arr, depth=1)) # [1, 2, 3, [4], 5]
+    # print(flatten(arr, depth=2)) # [1, 2, 3, 4, 5]
---
+++ 4/deprecated.py
@@ -0,0 +1,53 @@
+import functools
+
+
+def deprecated(f=None, since=None, will_be_removed=None):
+
+    if f is None:
+        return functools.partial(deprecated, since=since, will_be_removed=will_be_removed)
+
+    def inner(*args, **kwargs):
+        warning = f'Warning: function {f.__name__} is deprecated'
+
+        if since is not None:
+            warning += f' since version {since}.'
+        else:
+            warning += f'.'
+
+        if will_be_removed is not None:
+            warning += f' It will be removed in version {will_be_removed}'
+        else:
+            warning += f' It will be removed in future versions'
+
+        print(warning)
+
+        return f(*args, **kwargs)
+
+    return inner
+
+
+@deprecated
+def foo():
+    print('since=None\nwill_be_removed=None\n')
+
+
+@deprecated(since='1.13.1')
+def bar():
+    print('since=1.13.1\nwill_be_removed=None\n')
+
+
+@deprecated(will_be_removed='2.1.0')
+def pupa():
+    print('since=None\nwill_be_removed=2.1.0\n')
+
+
+@deprecated(since='1.13.1', will_be_removed='2.1.0')
+def lupa():
+    print('since=1.13.1\nwill_be_removed=2.1.0\n')
+
+
+if __name__ == '__main__':
+    foo()
+    bar()
+    pupa()
+    lupa()
