10a216bf7bd748e410ba9acac6d70b9898331b17
sash00k <s.a.c.h.a.b@yandex.ru>
2022-12-06 16:06:44 +0400

    add 13_coroutine.py

---
+++ 13_coroutine.py
@@ -0,0 +1,27 @@
+# Написать декоратор @coroutine, который избавляет от необходимости
+# сначала вызывать один раз next перед вызовами send.
+
+def coroutine(f, *args, **kwargs):
+    def wrapper():
+        called_f = f(*args, **kwargs)
+        next(called_f)
+        return called_f
+
+    return wrapper
+
+@coroutine
+def storage():
+    values = set()
+    was_there = False
+    while True:
+        val = yield was_there
+        was_there = val in values
+        if not was_there:
+            values.add(val)
+
+
+if __name__ == '__main__':
+    st = storage()
+    print(st.send(42)) # False
+    print(st.send(42)) # True
+    print(st.send(28)) # False
